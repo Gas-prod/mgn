053d106242c00f2823764ee73bfe211820fd57a5
Gas-prod <62751705+Gas-prod@users.noreply.github.com>
2022-01-25 00:40:48 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ var array = window.location.href.split("#");
 var startPart = array[array.length - 1];
 
 function openMenu(){
-    menu.classList.remove("close")
     menu.classList.add("open");
+    menu.classList.remove("close");
     menuButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>';
     menuButton.setAttribute("onclick", "closeMenu()");
 }
